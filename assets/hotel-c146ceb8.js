9cb0a1b3f75edfbe19742b3e2755ce727cddf86c
Dyhless <108591328+Dyhless@users.noreply.github.com>
2023-03-05 17:54:19 +0000

    Deploying to gh-pages from @ Dyhless/team-project-mimino@06a6678b75cbdfb35c339c81b8548e6b107d4a47 🚀

--- assets/hotel-c146ceb8.js
+++
@@ -1,1 +0,0 @@
-import"./room-modal-ea79b097.js";$(".slider").slick({arrows:!0,dots:!1,slidesToShow:2,slidesToScroll:1});
---
+++ assets/slider-5852924e.js
@@ -0,0 +1,1 @@
+$(".slider").slick({arrows:!0,dots:!1,slidesToShow:2,slidesToScroll:1});
