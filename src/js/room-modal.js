f0921b0ca6976750798907d11c0e600b0bc13b05
Dmytro Medvediev <s_medvedev@outlook.com>
2023-03-08 10:20:44 +0100

    update modal

--- src/js/room-modal.js
+++ src/js/room-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-room-main-open]'),
-    closeModalBtn: document.querySelector('[data-modal-room-main-close]'),
-    modal: document.querySelector('[data-modal-room-main]'),
+    openModalBtn: document.querySelector('[data-modal-main-room-open]'),
+    closeModalBtn: document.querySelector('[data-modal-main-room-close]'),
+    modal: document.querySelector('[data-modal-main-room]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -14,75 +14,6 @@
     refs.modal.classList.toggle('is-hidden');
 
     const scrollLockMethod = !isModalOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-room-room-open]'),
-    closeModalBtn: document.querySelector('[data-modal-room-room-close]'),
-    modal: document.querySelector('[data-room-room-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-room-main-2-open]'),
-    closeModalBtn: document.querySelector('[data-modal-room-main-2-close]'),
-    modal: document.querySelector('[data-room-main-2-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-hotel-room-open]'),
-    closeModalBtn: document.querySelector('[data-modal-hotel-room-close]'),
-    modal: document.querySelector('[data-room-hotel-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   }
 })();
--- src/js/table-modal.js
+++ src/js/table-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-     openModalBtn: document.querySelector('[data-modal-table-main-open]'),
-    closeModalBtn: document.querySelector('[data-modal-main-close]'),
-    modal: document.querySelector('[data-main-modal]'),
+     openModalBtn: document.querySelector('[data-modal-main-table-open]'),
+    closeModalBtn: document.querySelector('[data-modal-main-table-close]'),
+    modal: document.querySelector('[data-modal-main-table]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -20,73 +20,3 @@
     bodyScrollLock[scrollLockMethod](document.body);
   }
 })();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-table-main-2-open]'),
-    closeModalBtn: document.querySelector('[data-modal-table-main-2-close]'),
-    modal: document.querySelector('[data-table-main-2-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-table-rest-1-open]'),
-    closeModalBtn: document.querySelector('[data-modal-rest-1-close]'),
-    modal: document.querySelector('[data-rest-1-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
-(() => {
-  const refs = {
-     openModalBtn: document.querySelector('[data-modal-table-rest-2-open]'),
-    closeModalBtn: document.querySelector('[data-modal-rest-2-close]'),
-    modal: document.querySelector('[data-rest-2-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    const isModalOpen =
-      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
-    refs.openModalBtn.setAttribute('aria-expanded', !isModalOpen);
-    refs.modal.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isModalOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  }
-})();
-
